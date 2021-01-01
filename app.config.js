7eac50b89386d0503b4f63e940eaa1bad341b7e1
Ismael Rodriguez <ismael.rm.1986@gmail.com>
2021-01-01 19:44:07 +0100

    feat: Add description for release

--- app.config.js
+++ app.config.js
@@ -1,8 +1,18 @@
 import "dotenv/config";
 
+const getDescription = (releaseChannel) => {
+  if (releaseChannel === undefined) return "Description for local";
+  if (releaseChannel.indexOf("development") !== -1) return "Description for development";
+  if (releaseChannel.indexOf("production") !== -1) return "Description for production";
+};
+
 export default ({ config }) => {
+  console.log(config);
+  const { releaseChannel } = config;
+
   return {
     ...config,
+    description: getDescription(releaseChannel),
     // All values in extra will be passed to your app.
     extra: {
       // server: process.env.EXPO_PRODUCTION_SERVER,
