d941c475900ac79944e852375f1fdb6c5d8242e8
Ismael Rodriguez <ismael.rm.1986@gmail.com>
2021-04-04 18:10:40 +0200

    feat(theme): :lipstick: Add aniamation in home screen

--- src/components/Menu/MenuItem/MenuItem.js
+++ src/components/Menu/MenuItem/MenuItem.js
@@ -25,12 +25,12 @@ const MenuItem = ({ name, image, slug }) => {
         </View>
       ) : null}
 
-      <Text style={stylesRNElements.textDescription}>
+      {/* <Text style={stylesRNElements.textDescription}>
         Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el
         texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se
         dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro
         de textos especimen.
-      </Text>
+      </Text> */}
       <Button title="Ver" onPress={navigateToCategory} buttonStyle={{ marginTop: 15 }} titleStyle={bigTitleButton} />
     </Card>
   );
--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,24 +1,60 @@
-import React from "react";
-import { StyleSheet, View, Image } from "react-native";
+import React, { useRef, useEffect } from "react";
+import { StyleSheet, View, Image, Animated, Dimensions } from "react-native";
 import logo from "/assets/umami.png";
+import { Text } from "react-native-elements";
+import { red } from "/styles/theme";
+
+const windowWidth = Dimensions.get("window").width;
+const widthRatioAspect = 0.54;
+const heightRatioAspect = 0.77;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "white",
+  },
+  image: {
+    width: parseInt(widthRatioAspect * windowWidth),
+    height: parseInt(widthRatioAspect * windowWidth * heightRatioAspect),
+    resizeMode: "cover",
+  },
+});
 
 const Home = () => {
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: "center",
-      justifyContent: "center",
-      backgroundColor: "white",
-    },
-    image: {
-      width: 250,
-      height: 250,
-    },
-  });
+  const heightPosition = useRef(new Animated.Value(0)).current;
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.sequence([
+      Animated.timing(heightPosition, {
+        toValue: -100,
+        duration: 2000,
+        useNativeDriver: true,
+      }).start(),
+
+      Animated.timing(fadeAnim, {
+        toValue: 1,
+        duration: 5000,
+        useNativeDriver: true,
+      }).start(),
+    ]);
+  }, [heightPosition, fadeAnim]);
 
   return (
     <View style={styles.container}>
-      <Image source={logo} style={styles.image} />
+      <Animated.View style={{ transform: [{ translateY: heightPosition }] }}>
+        <Image source={logo} style={styles.image} />
+      </Animated.View>
+      <Animated.View style={{ translateY: -80, opacity: fadeAnim }}>
+        <Text h3 style={{ color: red }}>
+          Hamburguesería gourmet
+        </Text>
+        <Text h3 style={{ color: red, textAlign: "center" }}>
+          858 69 33 02
+        </Text>
+      </Animated.View>
     </View>
   );
 };
