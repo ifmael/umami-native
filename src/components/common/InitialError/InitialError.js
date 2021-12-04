ba8ae26691f875f395891525fd37e637590fb901
ismael Rodriguez <ismael.rm.1986@gmail.com>
2021-12-04 08:20:19 +0100

    refactor(secrets): :alien: show secrets

--- src/components/common/InitialError/InitialError.js
+++ src/components/common/InitialError/InitialError.js
@@ -1,20 +1,36 @@
 import React from "react";
 import { View } from "react-native";
 import { Text } from "react-native-elements";
+import { server, token, mapsToken, placesToken } from "/constants";
+import { any } from "prop-types";
 
-const InitialError = () => {
+const propTypes = {
+  error: any,
+};
+
+const InitialError = ({ error }) => {
   return (
     <View style={{ flex: 1, justifyContent: "center", marginHorizontal: 15 }}>
-      <Text h4 style={{ marginBottom: 30 }}>
+      <Text>server: {server}</Text>
+      <Text>token: {token}</Text>
+      <Text>maps:{mapsToken}</Text>
+      <Text>places:{placesToken}</Text>
+      <Text>{process.env.SENTRY_ORG}</Text>
+      <Text>Error graphl</Text>
+      <Text>message: {error.message}</Text>
+      <Text>errorMesage:{error.clientErrors?.name}</Text>
+      {/* <Text h4 style={{ marginBottom: 30 }}>
         Lo sentimos, ha habido un problema al cargar la aplicación.
       </Text>
       <Text h4 style={{ marginBottom: 5 }}>
         Intentelo de nuevo o le recordamos que puede también estamos en:
       </Text>
       <Text h4>· Llamando al 858 69 33 02</Text>
-      <Text h4>· Plaza Pintor Velázquez 2</Text>
+      <Text h4>· Plaza Pintor Velázquez 2</Text> */}
     </View>
   );
 };
+
+InitialError.propTypes = propTypes;
 
 export default InitialError;
--- src/context/GlobalContext.js
+++ src/context/GlobalContext.js
@@ -48,7 +48,7 @@ const ContextProvider = ({ children }) => {
   return !fontsLoaded || loading ? (
     <AppLoading />
   ) : error ? (
-    <InitialError />
+    <InitialError error={error} />
   ) : (
     <GlobalContext.Provider
       value={{
