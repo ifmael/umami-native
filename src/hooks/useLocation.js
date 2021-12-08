8c49ee0aca39af9acc776fefd0d0d932f3dbb866
ismael Rodriguez <ismael.rm.1986@gmail.com>
2021-12-08 10:45:37 +0100

    fix(location): :globe_with_meridians: ask for location persmission to get the place not at the beginning

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -1,21 +1,12 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import * as Location from "expo-location";
 
 const useLocation = () => {
-  const [hasLocation, setHasLocation] = useState(null);
-
   useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== "granted") return;
+    () => {
       Location.installWebGeolocationPolyfill();
-
-      let location = await Location.getCurrentPositionAsync({});
-      setHasLocation(location);
-    })();
+    };
   }, []);
-
-  return hasLocation;
 };
 
 export default useLocation;
