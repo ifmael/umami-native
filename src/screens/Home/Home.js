ffa1b955814e8a1aaa0b0a08aed21f4bc2cce9d4
Ismael Rodriguez <ismael.rodriguezmoreno@amaris.com>
2023-10-24 13:18:55 +0200

    fix(reanimated): do not use reanimated to avoid fatal error

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,5 +1,13 @@
 import React, { useRef, useEffect } from "react";
-import { StyleSheet, View, Image, Animated, Dimensions, Linking, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Image,
+  // Animated,
+  Dimensions,
+  Linking,
+  TouchableOpacity,
+} from "react-native";
 import logo from "/assets/umami.png";
 import { Text, Icon } from "react-native-elements";
 import { red } from "/styles/theme";
@@ -28,24 +36,24 @@ const styles = StyleSheet.create({
 const phoneNumber = 858693302;
 
 const Home = () => {
-  const heightPosition = useRef(new Animated.Value(0)).current;
-  const fadeAnim = useRef(new Animated.Value(0)).current;
+  // const heightPosition = useRef(new Animated.Value(0)).current;
+  // const fadeAnim = useRef(new Animated.Value(0)).current;
 
-  useEffect(() => {
-    Animated.sequence([
-      Animated.timing(heightPosition, {
-        toValue: -100,
-        duration: 2000,
-        useNativeDriver: true,
-      }).start(),
+  // useEffect(() => {
+  //   Animated.sequence([
+  //     Animated.timing(heightPosition, {
+  //       toValue: -100,
+  //       duration: 2000,
+  //       useNativeDriver: true,
+  //     }).start(),
 
-      Animated.timing(fadeAnim, {
-        toValue: 1,
-        duration: 5000,
-        useNativeDriver: true,
-      }).start(),
-    ]);
-  }, [heightPosition, fadeAnim]);
+  //     Animated.timing(fadeAnim, {
+  //       toValue: 1,
+  //       duration: 5000,
+  //       useNativeDriver: true,
+  //     }).start(),
+  //   ]);
+  // }, [heightPosition, fadeAnim]);
 
   const call = () => {
     Linking.openURL(`tel:${phoneNumber}`);
@@ -57,56 +65,56 @@ const Home = () => {
 
   return (
     <View style={styles.container}>
-      <Animated.View style={{ transform: [{ translateY: heightPosition }] }}>
+      {/* <Animated.View style={{ transform: [{ translateY: heightPosition }] }}>
         <Image source={logo} style={styles.image} />
-      </Animated.View>
-      <Animated.View
+      </Animated.View> */}
+      {/* <Animated.View
         style={{ position: "absolute", top: windowHeigth / 2 - 20, opacity: fadeAnim, paddingHorizontal: 10 }}
+      > */}
+      <Text h3 style={{ color: red }} numberOfLines={1} adjustsFontSizeToFit>
+        Hamburguesería gourmet
+      </Text>
+      <TouchableOpacity
+        onPress={call}
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
       >
-        <Text h3 style={{ color: red }} numberOfLines={1} adjustsFontSizeToFit>
-          Hamburguesería gourmet
+        <Icon
+          type="font-awesome-5"
+          name="phone"
+          size={24}
+          color={red}
+          iconStyle={{ transform: [{ rotate: "120deg" }], marginTop: 4 }}
+        />
+        <Text h3 style={{ color: red, textAlign: "center" }}>
+          858 69 33 02
         </Text>
-        <TouchableOpacity
-          onPress={call}
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <Icon
-            type="font-awesome-5"
-            name="phone"
-            size={24}
-            color={red}
-            iconStyle={{ transform: [{ rotate: "120deg" }], marginTop: 4 }}
-          />
-          <Text h3 style={{ color: red, textAlign: "center" }}>
-            858 69 33 02
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={goToInstagram}
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <Icon
-            type="font-awesome-5"
-            name="instagram"
-            size={30}
-            color={red}
-            iconStyle={{ marginRight: 6, marginTop: 4 }}
-          />
-          <Text h3 style={{ color: red, textAlign: "center" }}>
-            umamisantafe
-          </Text>
-        </TouchableOpacity>
-      </Animated.View>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={goToInstagram}
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Icon
+          type="font-awesome-5"
+          name="instagram"
+          size={30}
+          color={red}
+          iconStyle={{ marginRight: 6, marginTop: 4 }}
+        />
+        <Text h3 style={{ color: red, textAlign: "center" }}>
+          umamisantafe
+        </Text>
+      </TouchableOpacity>
+      {/* </Animated.View> */}
     </View>
   );
 };
